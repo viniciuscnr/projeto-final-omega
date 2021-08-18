ae5358485986110a14b63da6771bfc249ec915de
Homero <homerao@gmail.com>
2021-08-18 07:36:46 -0300

    adicionando as variáveis de ambiente

---
+++ src/dbconfig/db.config.ts
@@ -0,0 +1,9 @@
+
+export const DATABASE_TYPE = process.env.DATABASE_TYPE
+export const DATABASE_HOST = process.env.DATABASE_HOST
+export const DB_PORT = process.env.DB_PORT
+export const DB_USERNAME = process.env.DB_USERNAME
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DATABASE_OMEGA = process.env.DATABASE_OMEGA
+
+
