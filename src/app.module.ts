5bf591d86da3568dc30d4cc722ed0d768c7e0ae3
Homero <homerao@gmail.com>
2021-08-18 10:01:00 -0300

    configurando variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProposalsModule } from './proposals/proposals.module';
-
+import {} from './dbconfig/db.config'
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'OmegaDB',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE_OMEGA,
       autoLoadEntities: true,
       synchronize: true,
     }),
